8bdf6e112702dd2721c4831cd50fe8ef6f56dd1f
Abhishek Sinha <email4abhi@gmail.com>
2021-09-27 21:40:08 +0530

    Added myAtoI funtion.

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ class Test
 {
 public:
 	Test() { cout << "C-tor" << endl; }
-	Test(int _v):v(_v) { "Parameter C-tor" << endl;}
+	Test(int _v):v(_v) { cout << "Parameter C-tor" << endl;}
 	~Test() { cout << "D-tor" << endl; }
 	void show() { cout << v << endl; }
 	void set(int _v) { v = _v; }
@@ -51,10 +51,32 @@ void test_unique_ptr()
 }
 
 
+int myAtoI(string value)
+{
+	int result = 0;
+
+	int size = value.size();
+
+	//Dry Run of Below :: value = 123
+
+	// i = 0 -> 0 * 10 + 49 - 48 -> 0 + 1 -> 1 #ASCII of 1 is 49 & '0' is 48
+
+	for(int i = 0; i < size; i++)
+	{
+		result = result * 10 + value[i] - '0'; // TIP :: 
+											   //	    - Multiply with 10 & Subsctact with ASCII '0' to get INTEGER equivalent
+	}
+
+	return result;
+}
+
+
+
 int main()
 {
 	cout << "Hello from Ubuntu 20.04 LTS " << endl;
 //	test_virtual();
-	test_unique_ptr();
+	//test_unique_ptr();
+	cout << myAtoI("123") << endl;
 	return 0;
 }
